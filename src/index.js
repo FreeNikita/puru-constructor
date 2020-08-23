0afb4c8a4d911889f69d7d94c45f633b9593d61a
nikita <freenikita95@gmail.com>
2020-08-23 14:48:49 +0300

    add basic logic

--- src/index.js
+++ src/index.js
@@ -1,2 +1,47 @@
+import { model } from './model'
+import './styles/main.css'
 
-console.log("I'm index")
+model.forEach( block => {
+    let html;
+    switch(block.type){
+        case 'title':
+            html = title(block)
+            break
+        case 'text':
+            html = text(block)
+            break
+        case 'textColumns':
+            html = textColumns(block)
+            break
+    }
+
+    document.querySelector('#site').insertAdjacentHTML('beforeend', html)
+} )
+
+function title(block) {
+    return `
+        <div class="row">
+            <div class="col-sm">
+                <h1>${block.value}</h1>
+            </div>
+        </div>
+    `
+}
+
+function text(block) {
+    return `
+        <div class="row">
+            <div class="col-sm">
+                <p>${block.value}</p>
+            </div>
+        </div>
+    `
+}
+
+function textColumns(block) {
+    return `
+        <div class="row">
+            ${block.value.map( value => `<div class="col-sm"><p>${value}</p></div>`).join('')}
+        </div>
+    `
+}
---
+++ src/model.js
@@ -0,0 +1,9 @@
+export const model = [
+    {type: 'title', value: 'Test Value', option: {}},
+    {type: 'text', value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur error expedita hic itaque maiores minus nulla numquam perferendis placeat sapiente!', option: {}},
+    {type: 'textColumns', value: [
+        'text 1',
+        'text 2',
+        'text 3',
+    ], option: {}}
+]
