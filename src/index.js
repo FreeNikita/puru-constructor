54e7da4295b66876b09b64396c9cf55e04231078
nikita <freenikita95@gmail.com>
2020-08-23 20:20:06 +0300

    rewrite logic

--- src/index.js
+++ src/index.js
@@ -1,47 +1,8 @@
-import { model } from './model'
+import {model} from './model'
 import './styles/main.css'
 
-model.forEach( block => {
-    let html;
-    switch(block.type){
-        case 'title':
-            html = title(block)
-            break
-        case 'text':
-            html = text(block)
-            break
-        case 'textColumns':
-            html = textColumns(block)
-            break
-    }
+const site = document.querySelector('#site')
 
-    document.querySelector('#site').insertAdjacentHTML('beforeend', html)
-} )
-
-function title(block) {
-    return `
-        <div class="row">
-            <div class="col-sm">
-                <h1>${block.value}</h1>
-            </div>
-        </div>
-    `
-}
-
-function text(block) {
-    return `
-        <div class="row">
-            <div class="col-sm">
-                <p>${block.value}</p>
-            </div>
-        </div>
-    `
-}
-
-function textColumns(block) {
-    return `
-        <div class="row">
-            ${block.value.map( value => `<div class="col-sm"><p>${value}</p></div>`).join('')}
-        </div>
-    `
-}
+model.forEach(block => {
+    site.insertAdjacentHTML('beforeend', block.toHTML())
+})
